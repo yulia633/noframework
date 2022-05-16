31274f7550a575d477e43c0e418620617ed758b7
yulia633 <ignat633yulia@yandex.ru>
2022-05-16 21:49:18 +0300

    add rendering intrface

--- src/FrontPage/Presentation/FrontPageController.php
+++ src/FrontPage/Presentation/FrontPageController.php
@@ -4,14 +4,22 @@ declare(strict_types=1);
 
 namespace SocialNews\FrontPage\Presentation;
 
+use SocialNews\Framework\Rendering\TemplateRenderer;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 final class FrontPageController
 {
+    private $templateRenderer;
+
+    public function __construct(TemplateRenderer $templateRenderer)
+    {
+        $this->templateRenderer = $templateRenderer;
+    }
+
     public function show(Request $request): Response
     {
-        $content = "Hello, {$request->get('name', 'visitor')}";
+        $content = 'Hello, ' . $request->get('name', 'visitor');
         return new Response($content);
     }
 }

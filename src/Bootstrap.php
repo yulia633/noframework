6727871a8b21968abf0159fa716fe4f69ef75e59
yulia633 <ignat633yulia@yandex.ru>
2022-05-16 21:27:07 +0300

    add routing

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -8,8 +8,40 @@ require ROOT_DIR . '/vendor/autoload.php';
 \Tracy\Debugger::enable();
 
 $request = \Symfony\Component\HttpFoundation\Request::createFromGlobals();
-$controller = new \SocialNews\FrontPage\Presentation\FrontPageController();
-$response = $controller->show($request);
+
+$dispatcher = \FastRoute\simpleDispatcher(
+    function (\FastRoute\RouteCollector $r) {
+        $routes = include(ROOT_DIR . '/src/Routes.php');
+        foreach ($routes as $route) {
+            $r->addRoute(...$route);
+        }
+    }
+);
+
+$routeInfo = $dispatcher->dispatch(
+    $request->getMethod(),
+    $request->getPathInfo()
+);
+switch ($routeInfo[0]) {
+    case \FastRoute\Dispatcher::NOT_FOUND:
+        $response = new \Symfony\Component\HttpFoundation\Response(
+            'Not found',
+            404
+        );
+        break;
+    case \FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        $response = new \Symfony\Component\HttpFoundation\Response(
+            'Method not allowed',
+            405
+        );
+        break;
+    case \FastRoute\Dispatcher::FOUND:
+        [$controllerName, $method] = explode('#', $routeInfo[1]);
+        $vars = $routeInfo[2];
+        $controller = new $controllerName;
+        $response = $controller->$method($request, $vars);
+        break;
+}
 
 if (!$response instanceof \Symfony\Component\HttpFoundation\Response) {
     throw new \Exception('Controller methods must return a Response object');
---
+++ src/Routes.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    [
+        'GET',
+        '/',
+        'SocialNews\FrontPage\Presentation\FrontPageController#show'
+    ],
+    [
+        'GET',
+        '/submit',
+        'SocialNews\Submission\Presentation\SubmissionController#show'
+    ],
+];

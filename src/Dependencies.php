35593be905ebbacb0c3411b3f6d7e5317a79d21c
yulia633 <ignat633yulia@yandex.ru>
2022-05-18 07:41:54 +0300

    using dbal

--- src/Dependencies.php
+++ src/Dependencies.php
@@ -7,7 +7,7 @@ use SocialNews\Framework\Rendering\TemplateRenderer;
 use SocialNews\Framework\Rendering\TwigTemplateRendererFactory;
 use SocialNews\Framework\Rendering\TemplateDirectory;
 use SocialNews\FrontPage\Application\SubmissionsQuery;
-use SocialNews\FrontPage\Infrastructure\MockSubmissionsQuery;
+use SocialNews\FrontPage\Infrastructure\DbalSubmissionsQuery;
 use Doctrine\DBAL\Connection;
 use SocialNews\Framework\Dbal\ConnectionFactory;
 use SocialNews\Framework\Dbal\DatabaseUrl;
@@ -34,7 +34,7 @@ $injector->delegate(Connection::class, function () use ($injector): Connection {
 
 $injector->share(Connection::class);
 
-$injector->alias(SubmissionsQuery::class, MockSubmissionsQuery::class);
+$injector->alias(SubmissionsQuery::class, DbalSubmissionsQuery::class);
 $injector->share(SubmissionsQuery::class);
 
 $injector->define(TemplateDirectory::class, [':rootDirectory' => ROOT_DIR]);
---
+++ src/FrontPage/Infrastructure/DbalSubmissionsQuery.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SocialNews\FrontPage\Infrastructure;
+
+use Doctrine\DBAL\Connection;
+use SocialNews\FrontPage\Application\Submission;
+use SocialNews\FrontPage\Application\SubmissionsQuery;
+
+final class DbalSubmissionsQuery implements SubmissionsQuery
+{
+    private $connection;
+
+    public function __construct(Connection $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    public function execute(): array
+    {
+        $qb = $this->connection->createQueryBuilder();
+        
+        $qb->addSelect('title');
+        $qb->addSelect('url');
+        $qb->from('submissions');
+        $qb->orderBy('creation_date', 'DESC');
+        $stmt = $qb->execute();
+        $rows = $stmt->fetchAll();
+
+        $submissions = [];
+
+        foreach ($rows as $row) {
+            $submissions[] = new Submission($row['url'], $row['title']);
+        }
+        return $submissions;
+    }
+}

e6486e83495200f5a9304dc1e543b0e52bec359b
yulia633 <ignat633yulia@yandex.ru>
2022-05-18 16:00:19 +0300

    add flas message

--- src/Framework/Rendering/TwigTemplateRendererFactory.php
+++ src/Framework/Rendering/TwigTemplateRendererFactory.php
@@ -5,7 +5,8 @@ declare(strict_types=1);
 namespace SocialNews\Framework\Rendering;
 
 use SocialNews\Framework\Csrf\StoredTokenReader;
-
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
 use \Twig\Loader\FilesystemLoader;
 use \Twig\Environment;
 use \Twig\TwigFunction;
@@ -16,12 +17,16 @@ final class TwigTemplateRendererFactory
 
     private $templateDirectory;
 
+    private $session;
+
     public function __construct(
         TemplateDirectory $templateDirectory,
-        StoredTokenReader $storedTokenReader
+        StoredTokenReader $storedTokenReader,
+        Session $session
     ) {
         $this->templateDirectory = $templateDirectory;
         $this->storedTokenReader = $storedTokenReader;
+        $this->session = $session;
     }
 
     public function create(): TwigTemplateRenderer
@@ -31,10 +36,17 @@ final class TwigTemplateRendererFactory
         ]);
 
         $twigEnvironment = new Environment($loader);
+
         $twigEnvironment->addFunction(
             new TwigFunction('get_token', function (string $key): string {
                 $token = $this->storedTokenReader->read($key);
                 return $token->toString();
+            })
+        );
+
+        $twigEnvironment->addFunction(
+            new TwigFunction('get_flash_bag', function (): FlashBagInterface {
+                return $this->session->getFlashBag();
             })
         );
 
--- src/Submission/Presentation/SubmissionController.php
+++ src/Submission/Presentation/SubmissionController.php
@@ -5,23 +5,29 @@ declare(strict_types=1);
 namespace SocialNews\Submission\Presentation;
 
 use SocialNews\Framework\Csrf\StoredTokenValidator;
-use Symfony\Component\HttpFoundation\Response;
+use SocialNews\Framework\Csrf\Token;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use SocialNews\Framework\Rendering\TemplateRenderer;
-
+use Symfony\Component\HttpFoundation\Session\Session;
 
 final class SubmissionController
 {
     private $templateRenderer;
 
     private $storedTokenValidator;
-    
+
+    private $session;
+
     public function __construct(
         TemplateRenderer $templateRenderer,
-        StoredTokenValidator $storedTokenValidator
+        StoredTokenValidator $storedTokenValidator,
+        Session $session
     ) {
         $this->templateRenderer = $templateRenderer;
         $this->storedTokenValidator = $storedTokenValidator;
+        $this->session = $session;
     }
 
     public function show(): Response
@@ -32,7 +38,21 @@ final class SubmissionController
 
     public function submit(Request $request): Response
     {
-        $content = $request->get('title') . ' - ' . $request->get('url');
-        return new Response($content);
+        $response = new RedirectResponse('/submit');
+        if (!$this->storedTokenValidator->validate(
+            'submission',
+            new Token((string)$request->get('token'))
+        )) {
+            $this->session->getFlashBag()->add('errors', 'Invalid token');
+            return $response;
+        }
+
+        // save the submission…
+
+        $this->session->getFlashBag()->add(
+            'success',
+            'Your URL was submitted successfully'
+        );
+        return $response;
     }
 }

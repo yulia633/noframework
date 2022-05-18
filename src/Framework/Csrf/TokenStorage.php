3068a4adb61ee9dd1ce4f9ec3642cd36ddf74066
yulia633 <ignat633yulia@yandex.ru>
2022-05-18 08:10:07 +0300

    add store token

---
+++ src/Framework/Csrf/TokenStorage.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SocialNews\Framework\Csrf;
+
+interface TokenStorage
+{
+    public function store(string $key, Token $token): void;
+    public function retrieve(string $key): ?Token;
+}

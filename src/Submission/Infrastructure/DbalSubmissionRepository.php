050daa71612ca8a5284c6394aa73e8b2c60f63de
yulia633 <ignat633yulia@yandex.ru>
2022-05-18 18:04:32 +0300

    add parameterized queries

--- src/Submission/Infrastructure/DbalSubmissionRepository.php
+++ src/Submission/Infrastructure/DbalSubmissionRepository.php
@@ -19,15 +19,17 @@ final class DbalSubmissionRepository implements SubmissionRepository
 
     public function add(Submission $submission): void
     {
-        $this->connection->exec("
-            INSERT INTO
-            submissions (id, title, url, creation_date)
-            VALUES (
-            '{$submission->getId()->toString()}',
-            '{$submission->getTitle()}',
-            '{$submission->getUrl()}',
-            '{$submission->getCreationDate()->format('Y-m-d H:i:s')}'
-            );
-        ");
+        $qb = $this->connection->createQueryBuilder();
+        $qb->insert('submissions');
+        $qb->values([
+            'id' => $qb->createNamedParameter($submission->getId()->toString()),
+            'title' => $qb->createNamedParameter($submission->getTitle()),
+            'url' => $qb->createNamedParameter($submission->getUrl()),
+            'creation_date' => $qb->createNamedParameter(
+                $submission->getCreationDate(),
+                'datetime'
+            ),
+        ]);
+        $qb->execute();
     }
 }

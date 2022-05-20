7b72b9b641737c72635754739d08e35e0503af6b
yulia633 <ignat633yulia@yandex.ru>
2022-05-20 10:04:06 +0300

    add register method and constructor dependencies to the controller

--- src/User/Presentation/RegistrationController.php
+++ src/User/Presentation/RegistrationController.php
@@ -6,19 +6,49 @@ namespace SocialNews\User\Presentation;
 
 use SocialNews\Framework\Rendering\TemplateRenderer;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 final class RegistrationController
 {
     private $templateRenderer;
-    
-    public function __construct(TemplateRenderer $templateRenderer)
-    {
+    private $registerUserFormFactory;
+    private $session;
+    public function __construct(
+        TemplateRenderer $templateRenderer,
+        RegisterUserFormFactory $registerUserFormFactory,
+        Session $session
+    ) {
         $this->templateRenderer = $templateRenderer;
+        $this->registerUserFormFactory = $registerUserFormFactory;
+        $this->session = $session;
     }
-
+    
     public function show(): Response
     {
         $content = $this->templateRenderer->render('Registration.html.twig');
         return new Response($content);
+    }
+
+    public function register(Request $request): Response
+    {
+        $response = new RedirectResponse('/register');
+        $form = $this->registerUserFormFactory->createFromRequest($request);
+
+        if ($form->hasValidationErrors()) {
+            foreach ($form->getValidationErrors() as $errorMessage) {
+                $this->session->getFlashBag()->add('errors', $errorMessage);
+            }
+            return $response;
+        }
+
+        // register the user
+
+        $this->session->getFlashBag()->add(
+            'success',
+            'Your account was created. You can now log in.'
+        );
+        return $response;
     }
 }

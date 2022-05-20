3e6999c9180fffb4254b87d7d084eece6cd16e94
yulia633 <ignat633yulia@yandex.ru>
2022-05-20 10:01:56 +0300

    add factory for registration

---
+++ src/User/Presentation/RegisterUserFormFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SocialNews\User\Presentation;
+
+use SocialNews\Framework\Csrf\StoredTokenValidator;
+use Symfony\Component\HttpFoundation\Request;
+
+final class RegisterUserFormFactory
+{
+    private $storedTokenValidator;
+
+    public function __construct(StoredTokenValidator $storedTokenValidator)
+    {
+        $this->storedTokenValidator = $storedTokenValidator;
+    }
+    
+    public function createFromRequest(Request $request): RegisterUserForm
+    {
+        return new RegisterUserForm(
+            $this->storedTokenValidator,
+            (string)$request->get('token'),
+            (string)$request->get('nickname'),
+            (string)$request->get('password')
+        );
+    }
+}

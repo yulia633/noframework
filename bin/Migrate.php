c337434a2af70ca57ba138848dea3e3e27f4ed31
yulia633 <ignat633yulia@yandex.ru>
2022-05-20 10:23:57 +0300

    update migration file

--- bin/Migrate.php
+++ bin/Migrate.php
@@ -2,15 +2,57 @@
 
 declare(strict_types=1);
 
-use Migrations\Migration202205172209;
-
 define('ROOT_DIR', dirname(__DIR__));
+
 require ROOT_DIR . '/vendor/autoload.php';
 
 $injector = include(ROOT_DIR . '/src/Dependencies.php');
-
 $connection = $injector->make('Doctrine\DBAL\Connection');
-$migration = new Migration202205172209($connection);
-$migration->migrate();
 
-echo 'Finished running migrations' . PHP_EOL;
+/**
+ * A function that returns the class names of all 
+ * migration files in chronological order.
+ */
+function getAvailableMigrations(): array
+{
+    $migrations = [];
+
+    foreach (new FilesystemIterator(ROOT_DIR . '/migrations') as $file) {
+        $migrations[] = $file->getBasename('.php');
+    }
+    return array_reverse($migrations);
+}
+
+/**
+ * A function that shows a selection to the user.
+ */
+function selectMigration(array $migrations): int
+{
+    echo "[0] All" . PHP_EOL;
+
+    foreach ($migrations as $key => $name) {
+        $index = $key + 1;
+        echo "[$index] $name" . PHP_EOL;
+    }
+
+    $selected = readline('Select the migration that you want to run: ');
+    $selectedKey = $selected - 1;
+
+    if ($selected !== '0' && !array_key_exists($selectedKey, $migrations)) {
+        exit('Invalid selection' . PHP_EOL);
+    }
+    return (int)$selected;
+}
+
+$migrations = getAvailableMigrations();
+$selected = selectMigration($migrations);
+
+foreach ($migrations as $key => $migration) {
+    if ($selected !== 0 && $selected !== $key + 1) {
+        continue;
+    }
+
+    $class = "Migrations\\$migration";
+    (new $class($connection))->migrate();
+    echo "Running $migration..." . PHP_EOL;
+}

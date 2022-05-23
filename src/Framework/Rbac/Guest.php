f5ccc8a89034550c931a30cfb97f4a35008d7410
yulia633 <ignat633yulia@yandex.ru>
2022-05-23 12:20:11 +0300

    add users classes

---
+++ src/Framework/Rbac/Guest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SocialNews\Framework\Rbac;
+
+final class Guest implements User
+{
+    public function hasPermission(Permission $permission): bool
+    {
+        return false;
+    }
+}
---
+++ src/Framework/Rbac/User.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SocialNews\Framework\Rbac;
+
+interface User
+{
+    public function hasPermission(Permission $permission): bool;
+}

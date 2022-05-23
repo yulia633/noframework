97994a4a318a97369390dc6f1162fda5b93d17a6
yulia633 <ignat633yulia@yandex.ru>
2022-05-23 11:37:44 +0300

    add an array of role permissions

---
+++ src/Framework/Rbac/Role.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SocialNews\Framework\Rbac;
+
+abstract class Role
+{
+    public function hasPermission(Permission $permission): bool
+    {
+        return in_array($permission, $this->getPermissions());
+    }
+    /**
+     * @return Permission[]
+     */
+    abstract protected function getPermissions(): array;
+}
